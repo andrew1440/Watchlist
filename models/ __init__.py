49287a8432a3efe9fdc9fe218c12d2a8b8476700
andrew1440 <drewodongo470@gmail.com>
2020-01-28 15:01:12 +0300

    first commit

---
+++ models/ __init__.py
@@ -0,0 +1,6 @@
+ Title - The name of the movie
+ Overview - A short description on the movie
+ image- The poster image for the movie
+ vote_average - Average rating of the movie
+ vote_count - How many people have rated the movie
+ id - The movie id
